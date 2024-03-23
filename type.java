cf2d6d1135dc267b247e1a1be79615ef8cc6ec2e
Manireddy4 <160880429+Manireddy4@users.noreply.github.com>
2024-03-23 15:44:53 +0530

    Add files via upload

---
+++ type.java
@@ -0,0 +1,13 @@
+class arr1 {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 40;
+        a[3] = 30;
+        a[4] = 50;
+
+        for (int i = 0; i < a.length; i++)
+            System.out.println(a[i] + "\t");
+    }
+}
